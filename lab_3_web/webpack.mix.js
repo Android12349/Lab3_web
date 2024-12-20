da9be8c30274ee86562d943aba02f28878c6269a
Android <Andreyka1239@list.ru>
2024-12-20 12:22:23 +0500

    minor fixes

--- lab_3_web/webpack.mix.js
+++ lab_3_web/webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
+mix.js('resources/js/index.js', 'public/js')
+   .sass('resources/sass/styles.scss', 'public/css')
    .copyDirectory('resources/fonts', 'public/fonts')
    .setResourceRoot('/')
    .version();
