f020e21764dc6b5a46663501cf82188f12a7e912
Android <Andreyka1239@list.ru>
2024-12-20 09:15:09 +0500

    init breeze

--- lab_3_web/routes/web.php
+++ lab_3_web/routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CityController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('cities/trashed', [CityController::class, 'trashed'])->name('cities.trashed');
-Route::patch('cities/{id}/restore', [CityController::class, 'restore'])->name('cities.restore');
-Route::resource('cities', CityController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
